74a8280589392dab18653769d9a08c73c080da1f
Jacob Ayers <ayerswebdev@gmail.com>
2017-05-14 18:58:30 -0500

    time and date are now updated using JavaScript, not hard-coded in

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
   updateTime();
-  console.log("JS is working");
+  updateDate();
+  var intervalTime = setInterval(updateTime, 1000);
+  var intervalDate = setInterval(updateDate, 60000);
 });
 
 function updateTime() {
-  $("#time").html("6:30 PM");
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  var suffix = (hours > 11) ? "PM" : "AM";
+  hours = (hours === 0 || hours > 12) ? hours % 12 : hours;
+  $("#time").html(hours + ":" + minutes + " " + suffix);
+}
+
+function updateDate() {
+  var now = new Date();
+  var month = now.getMonth() + 1;
+  var date = now.getDate();
+  var year = now.getFullYear();
+  $("#date").html(month + "/" + date + "/" + year);
 }
