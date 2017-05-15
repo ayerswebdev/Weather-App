c5f819b9863384aec50f48ae6fc2d269d81537f7
Jacob Ayers <ayerswebdev@gmail.com>
2017-05-14 19:34:41 -0500

    hoping that this update brings with it the ability to use the Geolocation API to get the location of the user, then use the Google Reverse Geolocation API to get a ton of information that I can then display on screen

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   updateTime();
   updateDate();
+  updateLocationWeather();
   var intervalTime = setInterval(updateTime, 1000);
   var intervalDate = setInterval(updateDate, 60000);
 });
@@ -21,4 +22,25 @@ function updateDate() {
   var date = now.getDate();
   var year = now.getFullYear();
   $("#date").html(month + "/" + date + "/" + year);
+}
+
+function updateLocationWeather() {
+  if(navigator.geolocation) {
+    var location = getLocation();
+    var lat = location[0];
+    var lon = location[1];
+
+    $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=AIzaSyD2tL_cbRms_PUZl1qXPZjAucNmyCNKWa4", function(json) {
+      console.log(json);
+    });
+  }
+
+  function getLocation() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
+      var location = [lat, lon];
+      return location;
+    });
+  }
 }
