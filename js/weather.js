f2621cc1d05a815bfd9854b07246775f1df97247
Jacob Ayers <jacobayers@CCSD-MBP-JacobAyers-2.local>
2017-05-16 07:47:32 -0500

    updated JS to make the code more modular - created specific functions to get the coordinates, get city/state information and display it, and get weather information and display it

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
   updateTime();
   updateDate();
-  updateLocationWeather();
+  updateLocationWeather(function(coords) {
+    console.log(coords);
+    updateLocation(coords.lat, coords.lon);
+    updateWeather(coords.lat, coords.lon);
+  });
   var intervalTime = setInterval(updateTime, 1000); //update time every second
   var intervalDate = setInterval(updateDate, 60000); //update date every minute
-  var intervalLocationWeather = setInterval(updateLocationWeather, 900000); //update location/weather every fifteen minutes
+  //var intervalLocationWeather = setInterval(updateLocationWeather, 900000); //update location/weather every fifteen minutes
 });
 
 //use Date object to get the current time and display it on screen
@@ -29,77 +33,82 @@ function updateDate() {
   $("#date").html(formattedDate);
 }
 
-function updateLocationWeather() {
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(pos) {
-      var lat = pos.coords.latitude;
-      var lon = pos.coords.longitude;
-      console.log("Latitude: " + lat + "; " + "Longitude: " + lon);
+function updateLocationWeather(callback) {
+  navigator.geolocation.getCurrentPosition(function(pos) {
+    var coords = {
+      lat: pos.coords.latitude,
+      lon: pos.coords.longitude
+    };
 
-      //use Google Maps Geolocation API to convert latitude and longitude to a city and state, then display on screen
-      $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=AIzaSyD2tL_cbRms_PUZl1qXPZjAucNmyCNKWa4", function(json) {
-        console.log(json);
-        var city = "", state = "", country = "";
+    callback(coords);
+  });
+}
 
-        //rural areas use the "locality" type to hold the city name; search the JSON for this and update city variable if found
-        for(var i = 0; i < json.results[0].address_components.length; i++) {
-          for(var j = 0; j < json.results[0].address_components[i].types.length; j++) {
-            if(json.results[0].address_components[i].types[j] === "locality") {
-              city = json.results[0].address_components[i].short_name;
-            }
-          }
+function updateLocation(lat, lon) {
+  //use Google Maps Geolocation API to convert latitude and longitude to a city and state, then display on screen
+  $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=AIzaSyD2tL_cbRms_PUZl1qXPZjAucNmyCNKWa4", function(json) {
+    console.log(json);
+    var city = "", state = "", country = "";
 
-          //urban areas use the "sublocality" type to hold the city name; if no city was found before, find it here and update city variable
-          if(city === "") {
-            for(var k = 0; k < json.results[0].address_components[i].types.length; k++) {
-              if(json.results[0].address_components[i].types[k] === "sublocality") {
-                city = json.results[0].address_components[i].short_name;
-              }
-            }
-          }
+    //rural areas use the "locality" type to hold the city name; search the JSON for this and update city variable if found
+    for(var i = 0; i < json.results[0].address_components.length; i++) {
+      for(var j = 0; j < json.results[0].address_components[i].types.length; j++) {
+        if(json.results[0].address_components[i].types[j] === "locality") {
+          city = json.results[0].address_components[i].short_name;
+        }
+      }
 
-          //the "administrative_area_level_1" type is used to hold the state's name; collect the postal abbreviation from here
-          for(var l = 0; l < json.results[0].address_components[i].types.length; l++) {
-            if(json.results[0].address_components[i].types[l] === "administrative_area_level_1") {
-              state = json.results[0].address_components[i].short_name;
-            }
-          }
-
-          for(var m = 0; m < json.results[0].address_components[i].types.length; m++) {
-            if(json.results[0].address_components[i].types[m] === "country") {
-              country = json.results[0].address_components[i].short_name;
-            }
+      //urban areas use the "sublocality" type to hold the city name; if no city was found before, find it here and update city variable
+      if(city === "") {
+        for(var k = 0; k < json.results[0].address_components[i].types.length; k++) {
+          if(json.results[0].address_components[i].types[k] === "sublocality") {
+            city = json.results[0].address_components[i].short_name;
           }
         }
+      }
 
-        //update the displayed location. If not in US display city and country; if in US display city and state
-        var formattedCSC = (country !== "US") ? city + ", " + country : city + ", " + state;
-        console.log(city + ", " + state + ", " + country);
-        $("#loc").html(formattedCSC);
-      });
-
-      var appID = "&appid=296c68960963db7daf6943cd88b7d6f0";
-
-      //collect weather data
-      $.getJSON("https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=Imperial" + appID,function(json) {
-        //console.log(json);
-        console.log(json);
-        var temp = json.main.temp;
-        temp = temp.toFixed(0); //don't need to be too precise on temp
-
-        //temperature of -0 wouldn't make sense to the user
-        if(temp == -0) {
-          temp = 0;
+      //the "administrative_area_level_1" type is used to hold the state's name; collect the postal abbreviation from here
+      for(var l = 0; l < json.results[0].address_components[i].types.length; l++) {
+        if(json.results[0].address_components[i].types[l] === "administrative_area_level_1") {
+          state = json.results[0].address_components[i].short_name;
         }
+      }
 
-        var conditions = json.weather[0].main;
-        var iconID = json.weather[0].icon;
+      //get country data from JSON
+      for(var m = 0; m < json.results[0].address_components[i].types.length; m++) {
+        if(json.results[0].address_components[i].types[m] === "country") {
+          country = json.results[0].address_components[i].short_name;
+        }
+      }
+    }
 
-        //log and display temperature and conditions information
-        console.log(temp + String.fromCharCode(176) + " F, " + conditions + ", " + iconID);
-        $("#temp").html(temp + String.fromCharCode(176) + " F");
-        $("#conditions").html(conditions);
-      });
-    });
-  }
+    //update the displayed location. If not in US display city and country; if in US display city and state
+    var formattedCSC = (country !== "US") ? city + ", " + country : city + ", " + state;
+    console.log(city + ", " + state + ", " + country);
+    $("#loc").html(formattedCSC);
+  });
+}
+
+function updateWeather(lat, lon) {
+  var appID = "&appid=296c68960963db7daf6943cd88b7d6f0";
+
+  //collect weather data
+  $.getJSON("https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=Imperial" + appID,function(json) {
+    //console.log(json);
+    console.log(json);
+    var temp = json.main.temp.toFixed(0); //get temp, rounded to nearest degree
+
+    //temperature of -0 wouldn't make sense to the user
+    if(temp == -0) {
+      temp = 0;
+    }
+
+    var conditions = json.weather[0].main;
+    var weatherID = json.weather[0].id;
+
+    //log and display temperature and conditions information
+    console.log(temp + String.fromCharCode(176) + " F, " + conditions + ", " + weatherID);
+    $("#temp").html(temp + String.fromCharCode(176) + " F");
+    $("#conditions").html(conditions);
+  });
 }
